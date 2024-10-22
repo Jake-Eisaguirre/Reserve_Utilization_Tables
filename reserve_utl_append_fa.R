5ec34d6512f1efbbc0de16a3115ee9ba035f47c9
Jake-Eisaguirre <eisaguirre@bren.ucsb.edu>
2024-10-22 09:04:20 -1000

    annontation created for IT to replicate

--- reserve_utl_append_fa.R
+++ reserve_utl_append_fa.R
@@ -1,114 +1,114 @@
-print("Script is starting...")
+print("Script is starting...")  # Prints a message indicating the start of the script
 
-
+# Check if 'librarian' package is installed, if not, install it and load it
 if (!require(librarian)){
   install.packages("librarian")
   library(librarian)
 }
 
-# librarian downloads, if not already downloaded, and reads in needed packages
+# Load necessary packages using librarian, ensuring they are installed if missing
 librarian::shelf(tidyverse, here, DBI, odbc, padr)
 
+# Define current and related dates for query
+current_date <- Sys.Date()  # Set current date to today's date
+week_prior <- current_date - 3  # Set date 3 days prior to today
+week_prior_pairing_date <- current_date - 7  # Set date 7 days prior to today
+fut_date <- Sys.Date() + 7  # Set a future date (7 days from today)
+previous_bid_period <- substr(as.character((current_date - 30)), 1, 7)  # Get previous month and year
+update_dt_rlv <- paste0((as.character(previous_bid_period)), "-25 00:00:00")  # Set relevant update date
+raw_date <- Sys.Date()  # Save today's date as raw_date
 
-
-current_date <- Sys.Date()  # Today's date
-week_prior <- current_date - 3  # Date three days prior
-week_prior_pairing_date <- current_date - 7  # Date seven days prior
-fut_date <-  Sys.Date() + 7
-
-previous_bid_period <- substr(as.character((current_date - 30)), 1, 7)
-update_dt_rlv <- paste0((as.character(previous_bid_period)), "-25 00:00:00")
-raw_date <- Sys.Date()
-
+# Try to connect to the Snowflake database with tryCatch to handle errors
 tryCatch({
   db_connection <- DBI::dbConnect(odbc::odbc(),
                                   Driver="SnowflakeDSIIDriver",
                                   Server="hawaiianair.west-us-2.azure.snowflakecomputing.com",
                                   WAREHOUSE="DATA_LAKE_READER",
                                   Database="ENTERPRISE",
-                                  UID= Sys.getenv("UID"), 
+                                  UID= Sys.getenv("UID"),  # User ID pulled from environment variable
                                   authenticator = "externalbrowser")
-  print("Database Connected!")
+  print("Database Connected!")  # Print success message if connected
 },
 error=function(cond) {
-  print("Unable to connect to Database.")
+  print("Unable to connect to Database.")  # Print error message if connection fails
 })
 
-# Set search_path
+# Set the schema search path to 'CREW_ANALYTICS'
 dbExecute(db_connection, "USE SCHEMA CREW_ANALYTICS")
 
-
+# Define query to retrieve master history data based on pairing date range
 q_master_history <- paste0("SELECT * FROM CT_MASTER_HISTORY WHERE PAIRING_DATE BETWEEN '", week_prior, "' AND '", fut_date, "';")
 
+# Execute query and read in the master history data, converting time and date columns to character type
 master_history_raw <- dbGetQuery(db_connection, q_master_history) %>%
   mutate(UPDATE_TIME = as.character(UPDATE_TIME),
          UPDATE_DATE = as.character(UPDATE_DATE))
 
-
-fa_ut_rlv <- master_history_raw %>% 
-  ungroup() %>% 
-  filter(CREW_INDICATOR == "FA") %>% 
-  filter(TRANSACTION_CODE %in% c("RSV", "RLV")) %>%
-  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%
-  filter(update_dt < update_dt_rlv) %>%
+# Filter data for flight attendants with transaction codes (RSV, RLV) and remove duplicates
+fa_ut_rlv <- master_history_raw %>%
+  ungroup() %>%
+  filter(CREW_INDICATOR == "FA") %>%  # Filter for flight attendants
+  filter(TRANSACTION_CODE %in% c("RSV", "RLV")) %>%  # Filter for RSV and RLV transaction codes
+  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%  # Create update_dt combining date and time
+  filter(update_dt < update_dt_rlv) %>%  # Filter for updates before a specific date
   group_by(CREW_ID, PAIRING_DATE, TRANSACTION_CODE) %>%
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
+  ungroup() %>%
+  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_DATE, PAIRING_POSITION, BID_PERIOD, BASE) %>%  # Select relevant columns
+  mutate(EQUIPMENT = "NA")  # Set EQUIPMENT as "NA"
+
+# Filter data for flight attendants with ASN transaction code and remove duplicates
+fa_ut_asn <- master_history_raw %>%
+  ungroup() %>%
+  filter(CREW_INDICATOR == "FA") %>%  # Filter for flight attendants
+  filter(TRANSACTION_CODE %in% c("ASN")) %>%  # Filter for ASN transaction code
+  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%  # Create update_dt combining date and time
+  group_by(CREW_ID, PAIRING_DATE, TRANSACTION_CODE) %>%
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
+  ungroup() %>%
+  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_NO, PAIRING_DATE, TO_DATE, PAIRING_POSITION, BID_PERIOD, BASE, update_dt)
+
+# Process ASN records for single pairing dates
+fa_ut_single <- fa_ut_asn %>%
+  group_by(CREW_ID, PAIRING_NO) %>%
+  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>%  # Mark single-day pairings
+  filter(single == 1) %>%  # Filter single-day pairings
+  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"), values_to = "DATE") %>%  # Reshape data from wide to long format
+  group_by(CREW_ID, TRANSACTION_CODE, DATE, PAIRING_NO) %>%
   mutate(temp_id = cur_group_id()) %>%
   filter(!duplicated(temp_id)) %>%
-  ungroup() %>% 
-  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE,
-         PAIRING_DATE, PAIRING_POSITION, BID_PERIOD, BASE) %>% 
-  mutate(EQUIPMENT = "NA")
+  ungroup() %>%
+  mutate(EQUIPMENT = "NA") %>%  # Set EQUIPMENT as "NA"
+  rename(PAIRING_DATE = DATE) %>%
+  select(!c(PAIRING_NO, update_dt, single, name, temp_id))  # Drop unnecessary columns
 
-fa_ut_asn <- master_history_raw %>% 
-  ungroup() %>% 
-  filter(CREW_INDICATOR == "FA") %>% 
-  filter(TRANSACTION_CODE %in% c("ASN")) %>% 
-  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%
-  group_by(CREW_ID, PAIRING_DATE, TRANSACTION_CODE) %>%
+# Process ASN records for non-single pairing dates (doubles)
+fa_ut_double <- fa_ut_asn %>%
+  group_by(CREW_ID, PAIRING_NO) %>%
+  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>%  # Mark single-day pairings
+  filter(single == 0) %>%  # Filter non-single-day pairings
+  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"), values_to = "DATE") %>%
+  group_by(CREW_ID, TRANSACTION_CODE, DATE, PAIRING_NO) %>%
   mutate(temp_id = cur_group_id()) %>%
   filter(!duplicated(temp_id)) %>%
-  ungroup() %>% 
-  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_NO,
-         PAIRING_DATE, TO_DATE, PAIRING_POSITION, BID_PERIOD, BASE, update_dt)
-
-
-fa_ut_single <- fa_ut_asn %>% 
-  group_by(CREW_ID, PAIRING_NO) %>% 
-  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>% 
-  filter(single == 1) %>% 
-  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"),
-               values_to = "DATE") %>% 
-  group_by(CREW_ID, TRANSACTION_CODE, DATE, PAIRING_NO) %>% 
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>%
-  ungroup() %>% 
-  mutate(EQUIPMENT = "NA") %>% 
-  rename(PAIRING_DATE = DATE) %>% 
-  select(!c(PAIRING_NO, update_dt, single, name, temp_id))
-
-
-fa_ut_double <- fa_ut_asn %>% 
-  group_by(CREW_ID, PAIRING_NO) %>% 
-  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>% 
-  filter(single == 0) %>% 
-  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"),
-               values_to = "DATE") %>% 
-  group_by(CREW_ID, TRANSACTION_CODE, DATE, PAIRING_NO) %>% 
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>%
-  ungroup() %>% 
-  group_by(CREW_ID, BASE, PAIRING_NO) %>% 
-  pad() %>% 
-  ungroup() %>% 
-  mutate(EQUIPMENT = "NA")%>% 
-  rename(PAIRING_DATE = DATE) %>% 
+  ungroup() %>%
+  group_by(CREW_ID, BASE, PAIRING_NO) %>%
+  pad() %>%  # Fill missing dates (pad time series)
+  ungroup() %>%
+  mutate(EQUIPMENT = "NA") %>%
+  rename(PAIRING_DATE = DATE) %>%
   select(!c(PAIRING_NO, update_dt, single, name, temp_id)) %>%
-  fill(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_POSITION, BID_PERIOD, BASE, .direction = "down")
+  fill(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_POSITION, BID_PERIOD, BASE, .direction = "down")  # Fill missing values
 
-fa_ut <- rbind(fa_ut_rlv, fa_ut_single, fa_ut_double) %>% 
-  filter(PAIRING_DATE <= fut_date) %>% 
-  mutate(TRANSACTION_CODE = if_else(TRANSACTION_CODE == "RSV", "RLV", TRANSACTION_CODE)) %>% 
-  mutate(EQUIPMENT = "NA") #%>% 
+# Combine flight attendant data (RLV, single, and double) into a single dataset
+fa_ut <- rbind(fa_ut_rlv, fa_ut_single, fa_ut_double) %>%
+  filter(PAIRING_DATE <= fut_date) %>%
+  mutate(TRANSACTION_CODE = if_else(TRANSACTION_CODE == "RSV", "RLV", TRANSACTION_CODE)) %>%
+  mutate(EQUIPMENT = "NA")  # Set EQUIPMENT as "NA"
+
+#%>% 
   ## Gold Tier Below
   
   # group_by(PAIRING_DATE, BASE, TRANSACTION_CODE) %>% 
@@ -125,10 +125,7 @@ fa_ut <- rbind(fa_ut_rlv, fa_ut_single, fa_ut_double) %>%
   # relocate(EQUIPMENT, .before = ASN)
 
 
-
-
-
-# Connect to the `PLAYGROUND` database and append data if necessary
+# Connect to the 'PLAYGROUND' database with tryCatch to handle errors
 tryCatch({
   db_connection_pg <- DBI::dbConnect(odbc::odbc(),
                                      Driver = "SnowflakeDSIIDriver",
@@ -137,34 +134,33 @@ tryCatch({
                                      Database = "PLAYGROUND",
                                      UID = "jacob.eisaguirre@hawaiianair.com",
                                      authenticator = "externalbrowser")
-  print("Database Connected!")
+  print("Database Connected!")  # Print success message if connected
 }, error = function(cond) {
-  print("Unable to connect to Database.")
+  print("Unable to connect to Database.")  # Print error message if connection fails
 })
 
-# Set schema and retrieve data from `AA_FINAL_PAIRING` table
+# Set schema and query the 'AA_RESERVE_UTILIZATION' table
 dbExecute(db_connection_pg, "USE SCHEMA CREW_ANALYTICS")
 present_ut <- dbGetQuery(db_connection_pg, "SELECT * FROM AA_RESERVE_UTILIZATION")
 
-
-# Find matching columns between the present and final pairings
+# Find matching columns between present data and the new dataset
 matching_cols <- dplyr::intersect(colnames(present_ut), colnames(fa_ut))
 
-# Filter both datasets to have matching columns and append new records
+# Select matching columns from the present and new datasets
 match_present_fo <- present_ut %>%
   select(matching_cols)
 
 final_append_match_cols <- fa_ut %>%
   select(matching_cols)
 
-
+# Perform an anti-join to find records that need to be appended to the database
 final_append <- anti_join(final_append_match_cols, match_present_fo, by = join_by(PAIRING_POSITION, PAIRING_DATE, BASE, EQUIPMENT))
 
-# Append new records to the `AA_FINAL_PAIRING` table
+# Append the new records to the 'AA_RESERVE_UTILIZATION' table
 dbAppendTable(db_connection_pg, "AA_RESERVE_UTILIZATION", final_append)
 
 # Print the number of rows added and a success message
 print(paste(nrow(final_append), "rows added"))
-Sys.sleep(5)  # Pause for 10 seconds
-print("Script finished successfully!")
+Sys.sleep(5)  # Pause for 5 seconds
+print("Script finished successfully!")  # Indicate that the script completed
 Sys.sleep(10)  # Pause for 10 seconds
--- reserve_utl_append_p.R
+++ reserve_utl_append_p.R
@@ -1,144 +1,138 @@
-print("Script is starting...")
+print("Script is starting...")  # Prints message indicating the script is starting
 
-
+# Check if 'librarian' package is installed, if not, install it and load it
 if (!require(librarian)){
   install.packages("librarian")
   library(librarian)
 }
 
-# librarian downloads, if not already downloaded, and reads in needed packages
+# Load necessary packages using librarian, ensuring they are installed if missing
 librarian::shelf(tidyverse, here, DBI, odbc, padr)
 
+# Define dates for query
+current_date <- Sys.Date()  # Set current date to today's date
+week_prior <- current_date - 3  # Set date 3 days prior to today
+week_prior_pairing_date <- current_date - 7  # Set date 7 days prior to today
+previous_bid_period <- substr(as.character((current_date)), 1, 7)  # Get year and month as the previous bid period
+fut_date <- Sys.Date() + 7  # Set a future date (7 days from today)
+raw_date <- Sys.Date()  # Save today's date as raw_date
 
-current_date <- Sys.Date()  # Today's date
-week_prior <- current_date - 3  # Date three days prior
-week_prior_pairing_date <- current_date - 7  # Date seven days prior
-previous_bid_period <- substr(as.character((current_date)), 1, 7)
-
-fut_date <-  Sys.Date() + 7
-
-
-raw_date <- Sys.Date()
-
-
+# Try to connect to the Snowflake database with tryCatch to handle errors
 tryCatch({
   db_connection <- DBI::dbConnect(odbc::odbc(),
                                   Driver="SnowflakeDSIIDriver",
                                   Server="hawaiianair.west-us-2.azure.snowflakecomputing.com",
                                   WAREHOUSE="DATA_LAKE_READER",
                                   Database="ENTERPRISE",
-                                  UID= Sys.getenv("UID"), 
+                                  UID= Sys.getenv("UID"),  # User ID pulled from environment variable
                                   authenticator = "externalbrowser")
-  print("Database Connected!")
+  print("Database Connected!")  # Print success message if connected
 },
 error=function(cond) {
-  print("Unable to connect to Database.")
+  print("Unable to connect to Database.")  # Print error message if connection fails
 })
 
-# Set search_path
+# Set the schema search path to 'CREW_ANALYTICS'
 dbExecute(db_connection, "USE SCHEMA CREW_ANALYTICS")
 
-
+# Define query to retrieve master history data based on pairing date range
 q_master_history <- paste0("SELECT * FROM CT_MASTER_HISTORY WHERE PAIRING_DATE BETWEEN '", week_prior, "' AND '", fut_date, "';")
 
+# Execute query and read in the master history data, converting time and date columns to character type
 master_history_raw <- dbGetQuery(db_connection, q_master_history) %>%
   mutate(UPDATE_TIME = as.character(UPDATE_TIME),
          UPDATE_DATE = as.character(UPDATE_DATE))
 
-
-
+# Filter data for pilots with specific transaction codes (ARC, SCR) and remove duplicates
 pilot_ut_scr <- master_history_raw %>% 
   ungroup() %>% 
-  filter(CREW_INDICATOR == "P") %>% 
-  filter(TRANSACTION_CODE %in% c("ARC", "SCR")) %>% 
-  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%
+  filter(CREW_INDICATOR == "P") %>%  # Filter for pilots
+  filter(TRANSACTION_CODE %in% c("ARC", "SCR")) %>%  # Filter for ARC and SCR transaction codes
+  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%  # Create update_dt combining date and time
   group_by(CREW_ID, PAIRING_DATE, TRANSACTION_CODE) %>%
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>% 
-  ungroup() %>% 
-  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE,
-         PAIRING_DATE, TO_DATE, PAIRING_POSITION, BID_PERIOD, BASE)
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
+  ungroup() %>%
+  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_DATE, TO_DATE, PAIRING_POSITION, BID_PERIOD, BASE)
 
-
+# Filter data for pilots with ASN transaction code and remove duplicates
 pilot_ut_asn <- master_history_raw %>% 
   ungroup() %>% 
-  filter(CREW_INDICATOR == "P") %>% 
-  filter(TRANSACTION_CODE %in% c("ASN")) %>% 
-  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%
+  filter(CREW_INDICATOR == "P") %>%  # Filter for pilots
+  filter(TRANSACTION_CODE %in% c("ASN")) %>%  # Filter for ASN transaction code
+  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%  # Create update_dt combining date and time
   group_by(CREW_ID, PAIRING_DATE, TRANSACTION_CODE) %>%
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>% 
-  ungroup() %>% 
-  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_NO,
-         PAIRING_DATE, TO_DATE, PAIRING_POSITION, BID_PERIOD, BASE)
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
+  ungroup() %>%
+  select(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_NO, PAIRING_DATE, TO_DATE, PAIRING_POSITION, BID_PERIOD, BASE)
 
-
-
+# Query to get master schedule data for the previous bid period
 q_master_sched <- paste0("select CREW_ID, EFFECTIVE_FROM_DATE, EFFECTIVE_TO_DATE, EQUIPMENT, PAIRING_POSITION, BID_DATE,
                    UPDATE_DATE, UPDATE_TIME, BASE, BID_TYPE
                    from CT_MASTER_SCHEDULE WHERE BID_DATE = '", previous_bid_period, "';")
 
-
+# Execute query and read in the master schedule data
 raw_ms <- dbGetQuery(db_connection, q_master_sched)
 
+# Clean the master schedule data: remove duplicates, keep latest records, and rename columns
 clean_ms <- raw_ms %>% 
-  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>% 
+  mutate(update_dt = paste(UPDATE_DATE, UPDATE_TIME, sep = " ")) %>%  # Create update_dt combining date and time
   group_by(CREW_ID, EFFECTIVE_FROM_DATE, EFFECTIVE_TO_DATE, PAIRING_POSITION) %>% 
-  filter(update_dt == max(update_dt)) %>% 
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>% 
-  rename(BID_PERIOD = BID_DATE) %>% 
-  ungroup() %>% 
-  select(CREW_ID, BID_PERIOD, PAIRING_POSITION, EQUIPMENT, BASE)
+  filter(update_dt == max(update_dt)) %>%  # Keep the latest record for each group
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
+  rename(BID_PERIOD = BID_DATE) %>%  # Rename BID_DATE to BID_PERIOD
+  ungroup() %>%
+  select(CREW_ID, BID_PERIOD, PAIRING_POSITION, EQUIPMENT, BASE)  # Select relevant columns
 
-
+# Join and filter pilot SCR transaction data with cleaned master schedule data
 emp_hist_p_scr <-  pilot_ut_scr %>% 
-  inner_join(clean_ms, by = c("CREW_ID", "BID_PERIOD", "PAIRING_POSITION", "BASE")) %>% 
-  filter(PAIRING_POSITION %in% c("CA", "FO")) %>% 
-  select(!c(TO_DATE))
+  inner_join(clean_ms, by = c("CREW_ID", "BID_PERIOD", "PAIRING_POSITION", "BASE")) %>%  # Inner join on multiple columns
+  filter(PAIRING_POSITION %in% c("CA", "FO")) %>%  # Filter for Captain and First Officer
+  select(!c(TO_DATE))  # Drop the TO_DATE column
 
-
+# Join and filter pilot ASN transaction data with cleaned master schedule data
 emp_hist_p_asn <-  pilot_ut_asn %>% 
-  inner_join(clean_ms, by = c("CREW_ID", "BID_PERIOD", "PAIRING_POSITION", "BASE")) %>% 
-  filter(PAIRING_POSITION %in% c("CA", "FO")) 
+  inner_join(clean_ms, by = c("CREW_ID", "BID_PERIOD", "PAIRING_POSITION", "BASE")) %>%  # Inner join on multiple columns
+  filter(PAIRING_POSITION %in% c("CA", "FO"))  # Filter for Captain and First Officer
 
-
+# Process ASN records with different pairing and to dates (doubles), pad missing dates
 asn_double <- emp_hist_p_asn %>%
-  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>% 
-  filter(single == 0) %>% 
-  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"),
-               values_to = "DATE") %>% 
+  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>%  # Mark rows where PAIRING_DATE equals TO_DATE
+  filter(single == 0) %>%  # Filter for non-single-day pairings
+  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"), values_to = "DATE") %>%  # Reshape data from wide to long format
   group_by(CREW_ID, EQUIPMENT, TRANSACTION_CODE, DATE) %>% 
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>% 
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
   ungroup() %>% 
   group_by(CREW_ID, TRANSACTION_CODE, EQUIPMENT, PAIRING_NO) %>% 
-  pad(by='DATE') %>% 
-  ungroup()%>% 
-  rename(PAIRING_DATE = DATE) %>% 
-  select(!c(PAIRING_NO, single, name, temp_id))  %>%
-  fill(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_POSITION, BID_PERIOD, BASE, .direction = "down")
+  pad(by='DATE') %>%  # Fill in missing dates (pad time series)
+  ungroup() %>% 
+  rename(PAIRING_DATE = DATE) %>%  # Rename DATE to PAIRING_DATE
+  select(!c(PAIRING_NO, single, name, temp_id))  %>%  # Drop unnecessary columns
+  fill(CREW_INDICATOR, CREW_ID, TRANSACTION_CODE, PAIRING_POSITION, BID_PERIOD, BASE, .direction = "down")  # Fill missing values
 
+# Process ASN records for single pairing dates
 asn_single <- emp_hist_p_asn %>%
-  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>% 
-  filter(single == 1) %>% 
-  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"),
-               values_to = "DATE") %>% 
+  mutate(single = if_else(PAIRING_DATE == TO_DATE, 1, 0)) %>%  # Mark rows where PAIRING_DATE equals TO_DATE
+  filter(single == 1) %>%  # Filter for single-day pairings
+  pivot_longer(cols = c("PAIRING_DATE", "TO_DATE"), values_to = "DATE") %>%  # Reshape data from wide to long format
   group_by(CREW_ID, EQUIPMENT, TRANSACTION_CODE, DATE) %>% 
-  mutate(temp_id = cur_group_id()) %>% 
-  filter(!duplicated(temp_id)) %>% 
-  ungroup() %>% 
-  rename(PAIRING_DATE = DATE) %>% 
-  select(!c(PAIRING_NO, single, name, temp_id))
+  mutate(temp_id = cur_group_id()) %>%  # Assign unique ID to each group
+  filter(!duplicated(temp_id)) %>%  # Remove duplicates based on temp_id
+  ungroup() %>%
+  rename(PAIRING_DATE = DATE) %>%  # Rename DATE to PAIRING_DATE
+  select(!c(PAIRING_NO, single, name, temp_id))  # Drop unnecessary columns
 
-
-utl_p <- rbind(emp_hist_p_scr, asn_single, asn_double)%>% 
+# Combine SCR and ASN (single and double) pilot data into a single dataset
+utl_p <- rbind(emp_hist_p_scr, asn_single, asn_double) %>% 
   ungroup() %>% 
-  mutate(BASE = if_else(EQUIPMENT == "717", "HNL", BASE)) %>% 
-  filter(PAIRING_DATE <= fut_date) %>% 
-  mutate(TRANSACTION_CODE = if_else(TRANSACTION_CODE == "ACR", "SCR", TRANSACTION_CODE)) %>% 
-  filter(!EQUIPMENT == "33Y") #%>% 
-  
+  mutate(BASE = if_else(EQUIPMENT == "717", "HNL", BASE)) %>%  # Set BASE to HNL for 717 equipment
+  filter(PAIRING_DATE <= fut_date) %>%  # Filter data up to the future date
+  mutate(TRANSACTION_CODE = if_else(TRANSACTION_CODE == "ACR", "SCR", TRANSACTION_CODE)) %>%  # Replace "ACR" with "SCR"
+  filter(!EQUIPMENT == "33Y")  # Exclude records with equipment "33Y"
+
   ## Gold Tier Below
   # 
   # group_by(BASE, PAIRING_DATE, PAIRING_POSITION, EQUIPMENT, TRANSACTION_CODE) %>% 
@@ -155,7 +149,7 @@ utl_p <- rbind(emp_hist_p_scr, asn_single, asn_double)%>%
   # drop_na(RLV_SCR)
 
 
-# Connect to the `PLAYGROUND` database and append data if necessary
+# Connect to the 'PLAYGROUND' database
 tryCatch({
   db_connection_pg <- DBI::dbConnect(odbc::odbc(),
                                      Driver = "SnowflakeDSIIDriver",
@@ -164,35 +158,33 @@ tryCatch({
                                      Database = "PLAYGROUND",
                                      UID = "jacob.eisaguirre@hawaiianair.com",
                                      authenticator = "externalbrowser")
-  print("Database Connected!")
+  print("Database Connected!")  # Print success message if connected
 }, error = function(cond) {
-  print("Unable to connect to Database.")
+  print("Unable to connect to Database.")  # Print error message if connection fails
 })
 
-# Set schema and retrieve data from `AA_FINAL_PAIRING` table
+# Set schema and query the 'AA_RESERVE_UTILIZATION' table
 dbExecute(db_connection_pg, "USE SCHEMA CREW_ANALYTICS")
 present_ut <- dbGetQuery(db_connection_pg, "SELECT * FROM AA_RESERVE_UTILIZATION")
 
-
-# Find matching columns between the present and final pairings
+# Find matching columns between present data and the new dataset
 matching_cols <- dplyr::intersect(colnames(present_ut), colnames(utl_p))
 
-# Filter both datasets to have matching columns and append new records
+# Select matching columns from the present and new datasets
 match_present_fo <- present_ut %>%
   select(matching_cols)
 
 final_append_match_cols <- utl_p %>%
   select(matching_cols)
 
-
+# Perform an anti-join to find records that need to be appended to the database
 final_append <- anti_join(final_append_match_cols, match_present_fo, by = join_by(PAIRING_POSITION, PAIRING_DATE, EQUIPMENT))
 
-# Append new records to the `AA_FINAL_PAIRING` table
+# Append the new records to the 'AA_RESERVE_UTILIZATION' table
 dbAppendTable(db_connection_pg, "AA_RESERVE_UTILIZATION", final_append)
 
 # Print the number of rows added and a success message
 print(paste(nrow(final_append), "rows added"))
-Sys.sleep(5)  # Pause for 10 seconds
-print("Script finished successfully!")
+Sys.sleep(5)  # Pause for 5 seconds
+print("Script finished successfully!")  # Indicate that the script completed
 Sys.sleep(10)  # Pause for 10 seconds
-
